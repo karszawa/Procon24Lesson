39efc2e06f91b5c762824007141b2d57b096754b
siquare <cubicroonn@gmail.com>
2013-10-09 00:33:47 +0900

    Initial Commit

---
+++ ProconLesson/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProconLesson
+//
+//  Created by Cubic on 2013/10/09.
+//  Copyright (c) 2013年 Cubic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ProconLesson/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ProconLesson
+//
+//  Created by Cubic on 2013/10/09.
+//  Copyright (c) 2013年 Cubic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
