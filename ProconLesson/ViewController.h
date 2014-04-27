79c45a67fc65c20768e40da1e4f7dc0f729a3427
siquare <cubicroonn@gmail.com>
2014-04-27 12:22:47 +0900

    add README.md

--- ProconLesson/ViewController.h
+++ ProconLesson/ViewController.h
@@ -9,5 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    NSDate *tBegin;
+    NSTimer *timer;
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+
+- (void) makeImageView:(NSString *)path cgrect:(CGRect)cgrect tag:(int)tag;
+- (IBAction)setRandomDices:(id)sender;
+- (IBAction)pushStartButton:(id)sender;
+- (IBAction)pushStopButton:(id)sender;
+- (void) onTimer;
 
 @end
+
--- ProconLesson/ViewController.m
+++ ProconLesson/ViewController.m
@@ -14,16 +14,56 @@
 
 @implementation ViewController
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
-- (void)didReceiveMemoryWarning
+- (void) makeImageView:(NSString *)path cgrect:(CGRect)cgrect tag:(int)tag
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    UIImageView * image = [[UIImageView alloc] init];
+    image.frame = cgrect;
+    image.tag = tag;
+    image.image = [UIImage imageNamed:path];
+    [self.view addSubview:image];
+}
+
+- (IBAction)setRandomDices:(id)sender
+{
+    for(int y = 0; y < 6; y++) {
+        for(int x = 0; x < 9; x++) {
+            NSString *fileName = [NSString stringWithFormat : @"%d.png", rand() % 8 + 1];
+            [self makeImageView:fileName cgrect:CGRectMake(62+100*x, 25+100*y, 100, 100) tag:(y*10+x)];
+        }
+    }
+}
+
+- (IBAction)pushStartButton:(id)sender
+{
+    tBegin = [NSDate date];
+ 
+    if([timer isValid]) [timer invalidate];
+    timer = [NSTimer scheduledTimerWithTimeInterval:0.01f target:self selector:@selector(onTimer:) userInfo:_timerLabel repeats:YES];
+}
+
+- (IBAction)pushStopButton:(id)sender
+{
+    [timer invalidate];
+}
+
+- (void)onTimer:(NSTimer*)timer
+{
+    NSDate *now = [NSDate date];
+    float interval = [now timeIntervalSinceDate:tBegin];
+    int min = (int)(interval) / 60;
+    int sec = (int)(interval) % 60;
+    int unp = (int)(interval * 100) % 100;
+    
+    _timerLabel.text = [NSString stringWithFormat:@"%d:%02d:%02d", min, sec, unp];
 }
 
 @end
+
